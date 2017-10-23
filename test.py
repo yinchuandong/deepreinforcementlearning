04154ed7f3199798323385297b10ef228e5f9633
yinchuandong <johncdyin@gmail.com>
2017-10-23 13:42:06 +1100

    new: add custom flappybird

--- test.py
+++ test.py
@@ -43,9 +43,10 @@ def test1():
 def test2():
     logger = get_logger("tmp_dqn/tmp.log")
     process_fn = create_process_fn(is_atari=False, use_rgb=True)
-    env = Environment('FlappyBird', is_atari=False, frame_skip=10, display=True)
-    # env = Environment('Breakout-v0', is_atari=True, frame_skip=10, display=True)
-    nb_frames = 10
+    env = Environment('CustomFlappyBird', mode="custom", display=False)
+    # env = Environment('FlappyBird', mode="ple", frame_skip=10, display=True)
+    # env = Environment('Breakout-v0', mode="atari", frame_skip=10, display=True)
+    nb_frames = 100
     env.reset()
     for i in range(nb_frames):
         # Image.fromarray(np.reshape(o_t, [84, 84])).save("tmp_%d.png" % (i))
@@ -55,7 +56,7 @@ def test2():
         reward = np.clip(reward, -1.0, 1.0)
         o_t = process_fn(o_t)
         print(o_t.shape, o_t.dtype)
-        # Image.fromarray(o_t).save("tmp_%d.png" % (i))
+        Image.fromarray(o_t).save("tmp_%d.png" % (i))
         # print(reward)
         logger.info("{}".format(i))
         if terminal:
@@ -64,8 +65,21 @@ def test2():
     return
 
 
+def test3():
+    from customgame import CustomFlappyBird
+
+    # env = CustomFlappyBird(display_screen=False)
+    env = CustomFlappyBird(display_screen=True)
+    for _ in range(60):
+        action = np.random.randint(0, env.action_size)
+        o_t, reward, terminal = env.step(action)
+        print(action, reward, terminal)
+    return
+
+
 def main():
     test2()
+    # test3()
     return
 
 
--- util/env_util.py
+++ util/env_util.py
@@ -1,29 +1,36 @@
-import gym
-from gym.wrappers.frame_skipping import SkipWrapper
-from ple.games import *
-from ple import PLE
 import numpy as np
 
 
 class Environment(object):
 
-    def __init__(self, env_name, is_atari=True, display=True, frame_skip=1):
-        self.is_atari = is_atari
+    def __init__(self, env_name, mode="atari", display=True, frame_skip=1):
+        self.mode = mode
         self.display = display
 
-        if self.is_atari:
+        if self.mode == "atari":
             # atari game
+            import gym
+            from gym.wrappers.frame_skipping import SkipWrapper
             self.env = gym.make(env_name)
             skip_wrapper = SkipWrapper(frame_skip)
             self.env = skip_wrapper(self.env)
             self._action_size = self.env.action_space.n
             self._action_set = [k for k in range(self._action_size)]
-        else:
+        elif self.mode == "ple":
             # ple game
+            from ple.games import *
+            from ple import PLE
             game = eval(env_name)()
             self.env = PLE(game, fps=30, display_screen=display, frame_skip=frame_skip)
             self._action_size = len(self.env.getActionSet())
             self._action_set = self.env.getActionSet()
+        elif self.mode == "custom":
+            from customgame import *
+            self.env = eval(env_name)(display_screen=display, fps=60)
+            self._action_size = self.env.action_size
+            self._action_set = self.env.action_set
+        else:
+            raise Exception("mode can only be [atari, ple, custom]")
         return
 
     @property
@@ -35,23 +42,29 @@ class Environment(object):
         return self._action_set
 
     def step(self, action_idx):
-        if self.is_atari:
+        if self.mode == "atari":
             img, reward, terminal, _ = self.env.step(action_idx)
             if self.display:
                 self.env.render()
-        else:
+        elif self.mode == "ple":
             action = self.action_set[action_idx]
             reward = self.env.act(action)
             img = self.env.getScreenRGB()
             img = np.rot90(img, 3)
             terminal = self.env.game_over()
+        elif self.mode == "custom":
+            img, reward, terminal = self.env.step(action_idx)
+            img = np.rot90(img, 3)
         return (img, reward, terminal)
 
     def reset(self):
-        if self.is_atari:
+        if self.mode == "atari":
             img = self.env.reset()
-        else:
+        elif self.mode == "ple":
             self.env.reset_game()
             img = self.env.getScreenRGB()
+            img = np.rot90(img, 3)
+        elif self.mode == "custom":
+            img = self.env.reset()
             img = np.rot90(img, 3)
         return img
