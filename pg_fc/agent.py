8368fb27e55d57aec9fe2b65876d573143421af2
yinchuandong <johncdyin@gmail.com>
2017-11-06 10:06:15 +1100

    new: add fully-connected version for policy gradient

---
+++ pg_fc/agent.py
@@ -0,0 +1,175 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+import random
+from PIL import Image
+
+from base.base_agent import BaseAgent
+from util.network_util import restore_session, backup_session
+from util.feature_util import create_process_fn
+from util.data_util import minibatches
+from util.generic_util import Progbar
+from .network import Network
+
+
+class Agent(BaseAgent):
+
+    def __init__(self, cfg, logger):
+        BaseAgent.__init__(self)
+
+        self.cfg = cfg
+        self.logger = logger
+
+        # create networks
+        # state_chn = cfg.state_history * (3 if cfg.use_rgb else 1)
+        # input_shape = [cfg.state_dim, cfg.state_dim, state_chn]
+        input_shape = [cfg.state_dim]  # for cartpole
+        device = "/gpu:0" if cfg.use_gpu else "/cpu:0"
+        self.main_net = Network(input_shape, cfg.action_dim, "main_net", device)
+
+        self.logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        for var in self.main_net.vars:
+            print(var)
+        self.logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+        # create gradients operations
+        # optimizer = tf.train.RMSPropOptimizer(cfg.lr, decay=cfg.lr_decay)
+        optimizer = tf.train.AdamOptimizer(cfg.lr)
+        gradients = tf.gradients(self.main_net.loss, self.main_net.vars)
+        gradients_clipped = [tf.clip_by_norm(grad, cfg.max_grad) for grad in gradients]
+        self.apply_gradients = optimizer.apply_gradients(zip(gradients_clipped, self.main_net.vars))
+
+        self.stop_requested = False
+        self.global_t = 0
+        self.n_episode = 0
+        return
+
+    def add_train_summary(self, sess):
+        # tf.summary.scalar("loss", self.main_net.loss)
+        self.train_summary = tf.summary.merge_all()
+        self.train_summary_writer = tf.summary.FileWriter(self.cfg.log_dir + "/train", sess.graph)
+        return
+
+    def pick_action(self, sess, state):
+        pi_out = sess.run(self.main_net.pi, feed_dict={
+            self.main_net.states: [state], self.main_net.dropout: 1.0
+        })[0]
+        action_idx = np.random.choice(range(self.cfg.action_dim), p=pi_out)
+        return action_idx, pi_out
+
+    def _discount_reward(self, rewards, gamma=0.99):
+        discounted_r = np.zeros_like(rewards, dtype=np.float32)
+        running_add = 0
+        for t in reversed(range(len(rewards))):
+            # if rewards[t] < 0:  # for specific game
+            #     running_add = 0
+            running_add = rewards[t] + running_add * gamma
+            discounted_r[t] = running_add
+
+        discounted_r -= discounted_r.mean()
+        discounted_r /= discounted_r.std()
+        return discounted_r
+
+    def _update_weights(self, sess, minibatch):
+        batch_state, batch_action, batch_return = [], [], []
+        for s, a, r in minibatch:
+            batch_state.append(s)
+            batch_action.append(a)
+            batch_return.append(r)
+        _, loss = sess.run([self.apply_gradients, self.main_net.loss], feed_dict={
+            self.main_net.states: batch_state,
+            self.main_net.actions: batch_action,
+            self.main_net.returns: batch_return,
+            self.main_net.dropout: self.cfg.dropout,
+        })
+        return loss
+
+    def _run_episode(self, sess, epi_buffer):
+        n_batches = (len(epi_buffer) + self.cfg.batch_size - 1) // self.cfg.batch_size
+        prog = Progbar(target=n_batches)
+        states, actions, rewards = [], [], []
+        for s, a, r in epi_buffer:
+            states.append(s)
+            actions.append(a)
+            rewards.append(r)
+        returns = self._discount_reward(rewards, self.cfg.gamma)
+        epi_buffer = zip(states, actions, returns)
+
+        # print(returns)
+        # print(np.amax(states), np.amin(states))
+        # print(actions)
+        # print(len(actions))
+        # import sys
+        # sys.exit()
+        for i, minibatch in enumerate(minibatches(epi_buffer, self.cfg.batch_size, False)):
+            train_loss = self._update_weights(sess, minibatch)
+            prog.update(i + 1, [("train_loss", train_loss)])
+        # sys.exit()
+        return
+
+    def train(self, sess, env):
+        cfg = self.cfg
+        saver = tf.train.Saver(tf.global_variables())
+        # process_fn = create_process_fn(cfg.env_mode, cfg.use_rgb)
+
+        # summary
+        self.add_train_summary(sess)
+        self.global_t, self.n_episode = restore_session(saver, sess, cfg.model_dir)
+
+        epi_rewards = []
+        best_epi_reward = 0.0
+        while not self.stop_requested and self.global_t < cfg.max_train_step:
+            self.n_episode += 1
+            self.logger.info("\n-------new epoch: {}----------".format(self.n_episode))
+
+            o_t = env.reset()
+            # o_t = process_fn(o_t)
+            # s_t = np.concatenate([o_t] * self.cfg.state_history, axis=2)
+            s_t = o_t
+            done = False
+
+            epi_buffer = []
+            epi_reward = 0.0
+            while not done and not self.stop_requested and self.global_t < cfg.max_train_step:
+                self.global_t += 1
+                action, pi_out = self.pick_action(sess, s_t)
+                # o_t1, reward, done = env.step(action)
+                env.render()
+                o_t1, reward, done, _ = env.step(action)
+                # o_t1 = process_fn(o_t1)
+                # s_t1 = np.concatenate([s_t[:, :, 3 if cfg.use_rgb else 1:], o_t1], axis=2)
+                s_t1 = o_t1
+
+                epi_buffer.append((s_t, action, reward))
+                epi_reward += reward
+
+                s_t = s_t1
+                # if self.global_t % 100 == 0 or reward > 0.0:
+                if self.global_t % 100 == 0:
+                    self.logger.info("global_t={} / action_idx={} / reward={:04.2f} / pi={}"
+                                     .format(self.global_t, action, reward, str(pi_out)))
+
+            if self.stop_requested:
+                # skip the uncomplete episode
+                break
+            # train per episode
+            self._run_episode(sess, epi_buffer)
+
+            if self.global_t % 100000 == 0:
+                backup_session(saver, sess, cfg.model_dir, self.global_t, self.n_episode)
+
+            # save the episode reward
+            epi_rewards.append(epi_reward)
+            if len(epi_rewards) % 10 == 0:
+                self.logger.info("\n---episode={} / epi_buffer={} / avg_epi_reward={:04.2f}".
+                                 format(self.n_episode, len(epi_buffer), np.mean(epi_rewards)))
+                epi_rewards = []
+
+            if epi_reward > best_epi_reward:
+                self.logger.info("\n===reward improved from {:04.2f} to {:04.2f}".format(best_epi_reward, epi_reward))
+                best_epi_reward = epi_reward
+        backup_session(saver, sess, cfg.model_dir, self.global_t, self.n_episode)
+        return
---
+++ pg_fc/network.py
@@ -0,0 +1,84 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from util.network_util import *
+from base.base_network import BaseNetwork
+
+
+class Network(BaseNetwork):
+
+    def __init__(self, input_shape, action_dim, scope="net", device="/gpu:0"):
+        BaseNetwork.__init__(self, scope, device)
+        self._input_shape = input_shape
+        self._action_dim = action_dim
+        self._scope = scope
+        self._device = device
+        self._create_network()
+        self._prepare_loss()
+        return
+
+    def _create_network(self):
+        with tf.device(self._device), tf.variable_scope(self._scope):
+            self.states = tf.placeholder(tf.float32, shape=[None] + self._input_shape, name="states")
+            self.dropout = tf.placeholder(tf.float32, shape=[], name="dropout")
+
+            W_fc1, b_fc1 = fc_variable([self._input_shape[-1], 10], name="fc1")
+            h_fc1 = tf.nn.tanh(tf.matmul(self.states, W_fc1) + b_fc1)
+
+            h_fc1_dropout = tf.nn.dropout(h_fc1, self.dropout)
+
+            W_fc2, b_fc2 = fc_variable([10, self._action_dim], name="fc2")
+            h_fc2 = tf.matmul(h_fc1_dropout, W_fc2) + b_fc2
+
+            self.logits = h_fc2
+            self.pi = tf.nn.softmax(self.logits)
+        return
+
+    def _create_network2(self):
+        with tf.device(self._device), tf.variable_scope(self._scope):
+            self.states = tf.placeholder(tf.float32, shape=[None] + self._input_shape, name="states")
+            self.dropout = tf.placeholder(tf.float32, shape=[], name="dropout")
+
+            # state_dropout = tf.nn.dropout(self.states, self.dropout)
+            W_conv1, b_conv1 = conv_variable([8, 8, self._input_shape[2], 16], name="conv1")
+            h_conv1 = tf.nn.relu(conv2d(self.states, W_conv1, 4) + b_conv1)
+            # h_conv1 = tf.nn.relu(tf.layers.batch_normalization(conv2d(self.states, W_conv1, 4) + b_conv1))
+
+            # h_pool1 = max_pool_2x2(h_conv1)
+
+            W_conv2, b_conv2 = conv_variable([4, 4, 16, 32], name="conv2")
+            h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+            # h_conv2 = tf.nn.relu(tf.layers.batch_normalization(conv2d(h_pool1, W_conv2, 2) + b_conv2))
+
+            # W_conv3, b_conv3 = conv_variable([3, 3, 64, 64], name="conv3")
+            # h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
+            # h_conv3 = tf.nn.relu(tf.layers.batch_normalization(conv2d(h_conv2, W_conv3, 1) + b_conv3))
+
+            h_conv_flat_size, h_conv_flat = flatten_conv_layer(h_conv2)
+
+            W_fc1, b_fc1 = fc_variable([h_conv_flat_size, 256], name="fc1")
+            h_fc1 = tf.nn.relu(tf.matmul(h_conv_flat, W_fc1) + b_fc1)
+
+            h_fc1_dropout = tf.nn.dropout(h_fc1, self.dropout)
+
+            W_fc2, b_fc2 = fc_variable([256, self._action_dim], name="fc2")
+            h_fc2 = tf.matmul(h_fc1_dropout, W_fc2) + b_fc2
+
+            self.logits = h_fc2
+            self.pi = tf.nn.softmax(self.logits)
+        return
+
+    def _prepare_loss(self):
+        with tf.name_scope(self._scope):
+            self.actions = tf.placeholder(tf.int32, shape=[None], name="actions")
+            self.returns = tf.placeholder(tf.float32, shape=[None], name="returns")
+
+            action_onehot = tf.one_hot(self.actions, self._action_dim)
+            # _loss = self.returns * tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=action_onehot)
+            log_pi = tf.log(tf.clip_by_value(self.pi, 1e-20, 1.0))
+            _loss = -tf.reduce_sum(log_pi * action_onehot, axis=1) * self.returns
+            self.loss = tf.reduce_mean(_loss)
+        return
